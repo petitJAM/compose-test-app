fdc3881259c185d5eb160feb162853f5adf9f688
Alex Petitjean <alex@tenforwardconsulting.com>
2020-11-18 10:44:09 -0600

    Change UI to display typed text

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,29 @@
 import androidx.compose.desktop.Window
-import androidx.compose.material.Text
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 fun main() = Window {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var text by remember { mutableStateOf("") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column(Modifier.padding(16.dp)) {
+            TextField(
+                value = text,
+                modifier = Modifier.padding(vertical = 8.dp),
+                onValueChange = {
+                    text = it
+                })
+
+            Text("You entered: $text", modifier = Modifier.padding(vertical = 8.dp))
         }
     }
 }
